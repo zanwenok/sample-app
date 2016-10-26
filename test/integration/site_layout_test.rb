08c35dcf47de86322b119abf298cc6499af97e2a
codinghigh <buaazanwen@163.com>
2016-10-26 23:20:49 +0800

    Complete layout test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,35 @@
+# require 'test_helper'
+#
+# class SiteLayoutTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+#
+#   test "layout links" do
+#     get root_path
+#     assert_template 'static_pages/home'
+#     # assert_select "a[herf=?]", root_path
+#     assert_select "a[herf=?]", about_path
+#     assert_select "a[herf=?]", help_path
+#     assert_select "a[herf=?]", contact_path
+#     # get contact_path
+#     # assert_select "title", full_title("Contact")
+#   end
+#
+# end
+
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+end
