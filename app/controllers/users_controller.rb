81783ad0d9b3516cb85fdb8a6c3055dee32a2191
zanwen <buaazanwen@163.com>
2016-11-18 05:15:20 +0000

    login model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success]="Welcome to the Sample app!"
+      redirect_to @user  # same as redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
 end
